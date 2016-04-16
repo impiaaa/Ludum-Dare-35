8fdd747da85258da4140d8610f589011da65ca56
Spencer Alves <impiaaa@gmail.com>
2016-04-15 22:57:01 -0700

    Initial experiments

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public abstract class Mover : MonoBehaviour
+{
+    abstract public void Move(float move, bool crouch, bool jump);
+}
---
+++ Assets/Scripts/NoiseParticles.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoiseParticles : MonoBehaviour {
+    ParticleSystem psys;
+    ParticleSystem.Particle[] particles;
+
+    void Start () {
+        psys = GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[psys.maxParticles];
+    }
+
+    void LateUpdate() {
+        int numParticlesAlive = psys.GetParticles(particles);
+        for (int i = 0; i < numParticlesAlive; i++) {
+            Color c = particles[i].startColor;
+            if (c.r == 0.0) {
+                float gray = Random.value*Random.value;
+                particles[i].startColor = new Color(gray, gray, gray, c.a);
+            }
+        }
+        psys.SetParticles(particles, numParticlesAlive);
+    }
+}
